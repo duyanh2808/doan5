3ba0f2c8299d630295354f1af60fc779b5f416a3
duyanh2808 <70557142+duyanh2808@users.noreply.github.com>
2020-10-31 09:27:12 +0700

    udate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ChitietComponent } from './chitiet/chitiet.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { QlyteComponent } from './qlyte/qlyte.component';
+
+
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
-    
+   
   },
+  {
+    path:'chitiet/:id',
+    component: ChitietComponent,
+  },
+  {
+    path:'qlye',
+    component: QlyteComponent,
+  }
 ];
 
 @NgModule({
@@ -21,12 +34,15 @@ const routes: Routes = [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    ChitietComponent,
+    QlyteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
